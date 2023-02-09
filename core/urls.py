a45d1347699832cf5120881095c3b27e01b71ba9
Francis Angoring <bakitbilogangmundo@gmail.com>
2023-02-09 20:59:20 +0800

    login, signup & logout functionality

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name="landing-page"),
-    path('home/', views.index, name="index"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('signup/', views.signup_view, name="signup"),
+
+    path('home/', views.home, name="home"),
     path('profile/', views.profile, name="profile"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -6,9 +9,52 @@ def landing_page(request):
     return render(request, 'landing-page.html')
 
 
-def index(request):
+def login_view(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    context = {'page': page}
+    return render(request, 'login.html', context)
+
+
+def signup_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('landing-page')
+
+
+@login_required(login_url='login')
+def home(request):
     return render(request, 'core/index.html')
 
 
+@login_required(login_url='login')
 def profile(request):
     return render(request, 'core/profile.html')
